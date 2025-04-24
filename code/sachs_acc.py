3ca67f586b7b1a86135f3b033e5c1ff79e6fd639
jwkai <jamie.w.kai@gmail.com>
2025-04-24 14:10:35 -0700

    update scm in casuallearn

--- code/sachs_acc.py
+++ code/sachs_acc.py
@@ -11,8 +11,14 @@ import dataUtils
 import sys
 sys.path.append("causal-learn")
 
+from causallearn.graph.SHD import SHD
+from causallearn.graph.SCM import SCM
+
 
 sachs_ground = dataUtils.loadSachsGroundTruth()
+ground_SHD_sanity_check = SHD(sachs_ground, sachs_ground)
+print(f"SHD: {ground_SHD_sanity_check.get_shd()}")
+ground_SCM_sanity_check = SCM(sachs_ground, sachs_ground)
 
 sachsExpDataset = dataUtils.loadSachsDataset()
 sachsExpDataset = sachsExpDataset.astype({col: "float64" for col in sachsExpDataset.columns})
