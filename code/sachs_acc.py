41f3bb8a2b7daa79a5d74d7daf172f2af8784297
jwkai <jamie.w.kai@gmail.com>
2025-04-27 15:32:23 -0700

    update sachs_acc

--- code/sachs_acc.py
+++ code/sachs_acc.py
@@ -9,6 +9,7 @@ Created on Tue Apr 22 13:30:49 2025
 import dataUtils
 import utils
 import ccpg
+import ci_tests
 
 import sys
 sys.path.append("causal-learn")
@@ -18,33 +19,34 @@ from causallearn.graph.SCM import SCM
 
 sachs_ground = dataUtils.loadSachsGroundTruth()
 
-alpha=1e-4
-
 # Perform discrete data analysis
 disc_obs_data, disc_i_data, disc_i_nodes = dataUtils.loadSachsInterventionalDiscrete()
-d_cg, d_components, d_edges = ccpg.i_ccpg(
-                                disc_obs_data.to_numpy(),
-                                [i_dat.to_numpy() for i_dat in disc_i_data],
-                                disc_i_nodes,
-                                alpha=alpha,
-                                verbose=True,
-                                ci_test_name="gsq",
-                                node_names=list(disc_obs_data.columns))
+d_i_cg, d_i_components, d_i_edges = ccpg.i_ccpg(
+                                    disc_obs_data.to_numpy(),
+                                    [i_dat.to_numpy() for i_dat in disc_i_data],
+                                    disc_i_nodes,
+                                    alpha=0.05,
+                                    verbose=True,
+                                    ci_test_name="chisq",
+                                    node_names=list(disc_obs_data.columns))
 
-utils.plot_graph(d_cg, "sachs_discrete_i_ccpg")
+utils.plot_graph(d_i_cg, "sachs_discrete_i_ccpg")
 
-# Perform continuous data analysis
-cont_obs_data, cont_i_data, cont_i_nodes = dataUtils.loadSachsInterventionalContinuous()
-c_cg, c_components, c_edges = ccpg.i_ccpg(
-                                cont_obs_data.to_numpy(),
-                                [i_dat.to_numpy() for i_dat in cont_i_data],
-                                cont_i_nodes,
-                                alpha=alpha,
-                                verbose=True,
-                                ci_test_name="fisherz",
-                                node_names=list(cont_obs_data.columns))
+# ci_tests.register_ci_tests()
 
-x = 1
+# # Perform continuous data analysis
+# cont_obs_data, cont_i_data, cont_i_nodes = dataUtils.loadSachsInterventionalContinuous()
+# c_i_cg, c_i_components, c_i_edges = ccpg.i_ccpg(
+#                                     cont_obs_data.to_numpy(),
+#                                     [i_dat.to_numpy() for i_dat in cont_i_data],
+#                                     cont_i_nodes,
+#                                     threshold=0.0, #1e-4,
+#                                     verbose=True,
+#                                     ci_test_name="gaussbic",
+#                                     node_names=list(cont_obs_data.columns))
+
+# utils.plot_graph(c_i_cg, "sachs_continuous_i_ccpg")
+
 # ground_SHD_sanity_check = SHD(sachs_ground, sachs_ground)
 # print(f"SHD: {ground_SHD_sanity_check.get_shd()}")
 # ground_SCM_sanity_check = SCM(sachs_ground, sachs_ground)
